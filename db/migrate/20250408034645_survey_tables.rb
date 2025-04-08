0798ed24deb2b6826ba00366673f0a4e4b1fc797
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-08 09:23:18 +0530

    schema

--- db/migrate/20250408034645_survey_tables.rb
+++ db/migrate/20250408034645_survey_tables.rb
@@ -1,4 +1,34 @@
 class SurveyTables < ActiveRecord::Migration[8.0]
   def change
+    
+    create_table :surveys do |t|
+      t.string :name 
+      t.boolean :status, default: true 
+      t.timestamps 
+    end 
+
+    create_table :survey_questions do |t|
+      t.references :survey, foreign_key: true
+      t.string :question
+      t.string :question_type 
+      t.json :options 
+      t.boolean :status, default: true 
+      t.timestamps 
+    end 
+
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :contact 
+      t.timestamps 
+    end  
+
+    create_table :responses do |t|
+      t.references :survey
+      t.references :survey_question
+      t.references :user
+      t.string :response 
+      t.timestamps 
+    end  
   end
 end
